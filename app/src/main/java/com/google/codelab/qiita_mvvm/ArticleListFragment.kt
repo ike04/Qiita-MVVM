a16ea5b159f3feeeef5933c762fb712c8552143b
ike04 <boc.0409@outlook.jp>
2021-05-22 12:05:50 +0900

    Groupieの実装

--- app/src/main/java/com/google/codelab/qiita_mvvm/ArticleListFragment.kt
+++ app/src/main/java/com/google/codelab/qiita_mvvm/ArticleListFragment.kt
@@ -1,20 +1,53 @@
 package com.google.codelab.qiita_mvvm
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.codelab.qiita_mvvm.databinding.FragmentArticleBinding
+import androidx.fragment.app.Fragment
+import com.google.codelab.qiita_mvvm.databinding.FragmentArticleListBinding
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+
 
 class ArticleListFragment : Fragment() {
-    private lateinit var binding: FragmentArticleBinding
+    private lateinit var binding: FragmentArticleListBinding
+    private val groupAdapter = GroupAdapter<GroupieViewHolder>()
+    private val testData: MutableList<Article> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentArticleBinding.inflate(layoutInflater)
+        binding = FragmentArticleListBinding.inflate(layoutInflater)
+
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.recyclerView.adapter = groupAdapter
+        createTestData()
+        groupAdapter.update(testData.map { ArticleListItemFactory(it) })
+    }
+
+    private fun createTestData() {
+        var i = 0
+        while (i <= 10) {
+
+            testData.add(
+                Article(
+                    title = "Sample$i",
+                    likeCount = i
+                )
+            )
+            i += 1
+        }
+    }
 }
+
+data class Article(
+    val title: String,
+    val likeCount: Int
+)
